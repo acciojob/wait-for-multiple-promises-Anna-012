1d9bcdab5d0d040a4dbd367ed52d9a53fa024153
Anna shabbagh <afreenshabbagh@gmail.com>
2024-11-21 15:26:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let promise1 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("Promise 1 resolved");
+    },2000);
+})
+
+let promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("Promise 2 resolved");
+    },1000);
+})
+
+let promise3 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("Promise 3 resolved");
+    },3000);
+})
+
+Promise.all([promise1, promise2, promise3]).then((results)=>{
+     console.log(results)
+})
