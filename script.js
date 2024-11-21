17c5699f305ed8dad3a4bd45e902e144fbaadb36
Anna shabbagh <afreenshabbagh@gmail.com>
2024-11-21 15:52:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 //your JS code here. If required.
-let promise1 = new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-        resolve("Promise 1 resolved");
-    },2000);
-})
-
-let promise2 = new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-        resolve("Promise 2 resolved");
-    },1000);
-})
-
-let promise3 = new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-        resolve("Promise 3 resolved");
-    },3000);
-})
-
-Promise.all([promise1, promise2, promise3]).then((results)=>{
-     console.log(results)
-})
+window.onload = function () {
+  const outputElement = document.getElementById("output");
+ 
+  const loadingRow = document.createElement("tr");
+  loadingRow.setAttribute("id", "loading"); 
+  const loadingCell = document.createElement("td");
+  loadingCell.setAttribute("colspan", "2");
+  loadingCell.textContent = "Loading...";
+  loadingRow.appendChild(loadingCell);
+  outputElement.appendChild(loadingRow);
+ 
+  const startTime = Date.now();
+ 
+  function createPromise(i) {
+    const delay = Math.floor(Math.random() * 2000) + 1000; 
+    const promiseStartTime = Date.now();
+ 
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const timeTaken = (Date.now() - promiseStartTime) / 1000; // In seconds
+        resolve({ name: "Promise " + i, timeTaken: timeTaken.toFixed(3) });
+      }, delay);
+    });
+  }
+ 
+  const promises = [];
